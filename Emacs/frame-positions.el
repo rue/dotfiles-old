87f7ee58906377efe12ae6da33554f3c0a3c375e
Eero Saynatkari <projects@kittensoft.org>
2012-11-17 19:57:54 +0200

    Initial import of Aquamacs

---
+++ Emacs/frame-positions.el
@@ -0,0 +1,8 @@
+;; Saved Frame Positions
+
+
+(if (fboundp (quote smart-frame-set-initial-frame-alist)) (smart-frame-set-initial-frame-alist (quote ((left . 0) (top . 22) (width . 80) (height . 35)))))
+
+
+
+(setq smart-frame-prior-positions (quote nil))
