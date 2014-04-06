313004cc4028b99ad090c976f3768b5ed943aaad
Eero Saynatkari <projects@kittensoft.org>
2014-04-06 14:18:01 +0300

    Drop Emacs

--- Emacs/frame-positions.el
+++
@@ -1,8 +0,0 @@
-;; Saved Frame Positions
-
-
-(if (fboundp (quote smart-frame-set-initial-frame-alist)) (smart-frame-set-initial-frame-alist (quote ((left . 0) (top . 22) (width . 80) (height . 35)))))
-
-
-
-(setq smart-frame-prior-positions (quote nil))
